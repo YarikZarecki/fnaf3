960f7520e2900feec05c6304b2e680ac4be11262
Yaraslau Zaretski <01210284@pw.edu.pl>
2026-04-06 14:09:13 +0200

    C++ style

--- functions.hpp
+++ functions.hpp
@@ -1,25 +1,22 @@
-#ifndef FUNCTIONS_HPP
-#define FUNCTIONS_HPP
+#pragma once
 #include <iostream>
 
 inline int hour, hour_lenght;
-inline int night, springtrap_ai;
+inline int night, springtrap_ai, springtrap_position;
 
 inline int current_audio_uses, audio_uses_per_night;
 inline int current_camera_time, camera_time_per_night;
 inline int current_ventilation_time, ventilation_time_per_night;
 
-inline int opened_cam;
+inline int opened_cam, opened_vent_cam;
 inline bool vent_cams; //переменная правдива если открыты камеры с 11 по 15
 
-inline bool office_position, 
+inline bool office_position, maintenance_panel_opened, cameras_open;
 
 inline bool ventilation_broken = false, repairing = false;
 
 inline int cheat, activated_cheat, repair_number, player_change;
 inline bool fast_nights, agressive, radar, no_errors;//активация читов
-
-inline bool maintenance_panel_opened = false, cameras_open = false;
 
 //функции менюшки
 void wrong_number();
@@ -34,6 +31,3 @@ int rng();
 int time();
 int system_repair();
 void office_position();
-
-
-#endif
